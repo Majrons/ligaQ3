afce93ebb0b6bb9bbfe6fa8df52b98181214499d
Artur Muras <artur.muras@answear.com>
2024-09-29 21:30:23 +0200

    feature/NO_ISSUE-first-commit - added connection to db - added match model - added team model - added new match model - added circleci config

--- app.js
+++ app.js
@@ -5,17 +5,15 @@ const connectDB = require('./src/config/db');
 
 const app = express();
 
-// Połącz z bazą danych
 connectDB();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Route imports
 app.use('/api/teams', require('./src/routes/teams'));
 app.use('/api/matches', require('./src/routes/matches'));
 app.use('/api/auth', require('./src/routes/auth'));
+app.use('/api/admin', require('./src/routes/admin'));
 
 // Uruchomienie serwera
 const PORT = process.env.PORT || 5000;
