ab375b5a8c8a5c21e9c1612a4c5eeca29fac3a0b
Artur Muras <artur.muras@answear.com>
2024-09-30 13:02:13 +0200

    main/NO_ISSUE fix cors

--- app.js
+++ app.js
@@ -6,9 +6,16 @@ const connectDB = require('./src/config/db');
 
 const app = express();
 
+const corsOptions = {
+    origin: ['http://localhost:3000', 'https://liga-q3.pl'], // Dodaj URL swojej aplikacji front-endowej
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+};
+
 connectDB();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/api/teams', require('./src/routes/teams'));
