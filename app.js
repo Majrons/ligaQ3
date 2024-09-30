08e26d7b47f84a4191a683adecfa1a6a6d330ed7
Artur Muras <artur.muras@answear.com>
2024-09-30 13:36:02 +0200

    main/NO_ISSUE fix cors

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // app.js
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./src/config/db');
+require('dotenv').config();
 
 const app = express();
 
