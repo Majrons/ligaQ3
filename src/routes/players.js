d0a2bb0db3bf25c9f90d0169a7683f8ec9df5ad7
Artur Muras <artur.muras@answear.com>
2024-10-01 01:31:29 +0200

    feature/NO_ISSUE delete match

--- src/routes/players.js
+++ src/routes/players.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const playerController = require('../controllers/playerController');
 
 // Endpoint do dodawania nowego gracza
-router.post('/', playerController.addPlayer);
+router.post('/team/:teamId', playerController.addPlayer);
 
 // Endpoint do pobierania graczy z danej drużyny
 router.get('/team/:teamId', playerController.getPlayersByTeam);
