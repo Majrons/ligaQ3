ef37043b2970e1c1506694befd25436527a48435
Artur Muras <artur.muras@answear.com>
2024-10-02 22:33:05 +0200

    feature/NO_ISSUE delete match

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const matchController = require('../controllers/matchController');
 const verifyRole = require('../middlewares/authMiddleware');
 
-router.post('/', verifyRole, matchController.addMatch);
+router.post('/', verifyRole(['admin', 'mod']), matchController.addMatch);
 router.get('/', matchController.getAllMatches);
 router.get('/team/:teamId', matchController.getMatchesByTeam);
 router.put('/:id', verifyRole(['admin']),  matchController.updateMatch);
