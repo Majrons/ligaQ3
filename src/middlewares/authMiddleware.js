76548621e552f553ece9ef538561f75c8ab5176d
Artur Muras <artur.muras@answear.com>
2024-10-18 18:27:39 +0200

    feature/expired token

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,27 +1,32 @@
-// middlewares/authMiddleware.js
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-// Middleware sprawdzający rolę użytkownika
+// Middleware sprawdzający token i rolę użytkownika
 const verifyRole = (roles) => {
-    return (req, res, next) => {
-        const token = req.header('Authorization').replace('Bearer ', '');
+    return async (req, res, next) => {
+        const token = req.header('Authorization') ? req.header('Authorization').replace('Bearer ', '') : null;
         if (!token) return res.status(401).json({ msg: 'Brak autoryzacji, token wymagany' });
 
         try {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            req.user = decoded;
-        } catch (error) {
-            return res.status(401).json({ msg: 'Nieprawidłowy token' });
-        }
 
-        // Znajdź użytkownika po ID z tokenu
-        User.findById(req.user.id).then(user => {
+            // Sprawdzenie, czy token nie wygasł
+            if (decoded.exp * 1000 < Date.now()) {
+                return res.status(401).json({ msg: 'Sesja wygasła, zaloguj się ponownie' });
+            }
+
+            req.user = decoded;
+
+            // Znajdź użytkownika w bazie
+            const user = await User.findById(req.user.id);
             if (!user || !roles.includes(user.role)) {
                 return res.status(403).json({ msg: 'Brak uprawnień' });
             }
-            next();
-        });
+
+            next(); // Użytkownik ma odpowiednią rolę, kontynuujemy
+        } catch (error) {
+            return res.status(401).json({ msg: 'Nieprawidłowy token' });
+        }
     };
 };
 
