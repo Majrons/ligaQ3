3381172fe0b55a4fc0b7c5c048c9f36122713f6e
Artur Muras <artur.muras@answear.com>
2024-10-30 17:01:12 +0100

    feature/adding screenshots

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 const matchController = require('../controllers/matchController');
 const verifyRole = require('../middlewares/authMiddleware');
 
-// Ustawienia multer do przechowywania przesłanych plików
+// Ustawienia multer do przechowywania i walidacji plików
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'uploads/'); // Przechowywanie plików w folderze 'uploads'
@@ -14,7 +14,21 @@ const storage = multer.diskStorage({
         cb(null, Date.now() + '-' + file.originalname); // Unikalna nazwa pliku
     }
 });
-const upload = multer({ storage: storage });
+
+// Filtr plików, aby akceptować tylko JPG o rozmiarze do 1 MB
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg') {
+        cb(null, true);
+    } else {
+        cb(new Error('Niewłaściwy format pliku. Akceptowane są tylko pliki JPG.'));
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 1 * 1024 * 1024 }, // Maksymalny rozmiar pliku: 1 MB
+    fileFilter: fileFilter
+});
 
 // Trasy związane z meczami
 router.post(
