42135988688cb7e738473a46e366cb0aa85fc85f
Artur Muras <artur.muras@answear.com>
2024-10-30 19:38:46 +0100

    feature/adding screenshots

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -1,40 +1,40 @@
 // routes/matches.js
 const express = require('express');
 const router = express.Router();
-// const multer = require('multer');
+const multer = require('multer');
 const matchController = require('../controllers/matchController');
 const verifyRole = require('../middlewares/authMiddleware');
 
 // Ustawienia multer do przechowywania i walidacji plików
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, 'uploads/'); // Przechowywanie plików w folderze 'uploads'
-//     },
-//     filename: (req, file, cb) => {
-//         cb(null, Date.now() + '-' + file.originalname); // Unikalna nazwa pliku
-//     }
-// });
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/'); // Przechowywanie plików w folderze 'uploads'
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname); // Unikalna nazwa pliku
+    }
+});
 
 // Filtr plików, aby akceptować tylko JPG o rozmiarze do 1 MB
-// const fileFilter = (req, file, cb) => {
-//     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg') {
-//         cb(null, true);
-//     } else {
-//         cb(new Error('Niewłaściwy format pliku. Akceptowane są tylko pliki JPG.'));
-//     }
-// };
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg') {
+        cb(null, true);
+    } else {
+        cb(new Error('Niewłaściwy format pliku. Akceptowane są tylko pliki JPG.'));
+    }
+};
 
-// const upload = multer({
-//     storage: storage,
-//     limits: { fileSize: 1024 * 1024 }, // Maksymalny rozmiar pliku: 1 MB
-//     fileFilter: fileFilter
-// });
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 1024 * 1024 }, // Maksymalny rozmiar pliku: 1 MB
+    fileFilter: fileFilter
+});
 
 // Trasy związane z meczami
 router.post(
     '/',
     verifyRole(['admin', 'mod']),
-    // upload.fields([{ name: 'screenshot1' }, { name: 'screenshot2' }]),
+    upload.fields([{ name: 'screenshot1' }, { name: 'screenshot2' }]),
     matchController.addMatch
 );
 router.get('/', matchController.getAllMatches);
@@ -43,7 +43,7 @@ router.get('/:id', matchController.getMatchById);
 router.put(
     '/:id',
     verifyRole(['admin']),
-    // upload.fields([{ name: 'screenshot1' }, { name: 'screenshot2' }]),
+    upload.fields([{ name: 'screenshot1' }, { name: 'screenshot2' }]),
     matchController.updateMatch
 );
 router.delete('/:id', verifyRole(['admin']), matchController.deleteMatch);
